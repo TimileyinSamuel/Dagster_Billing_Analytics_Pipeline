0493aa30896c55d256e2b81addccf7ebc9356df6
Timmy Ajitoni <ajitonitimileyin@gmail.com>
2026-04-02 18:49:10 +0200

    create fact models

---
+++ src/billing_analytics_pipeline/assets/facts.py
@@ -0,0 +1,103 @@
+import dagster as dg
+from billing_analytics_pipeline.resources.duckdb_resource import DuckDBResource
+
+@dg.asset(
+    deps = ["int_location_metrics_weekly"]
+)
+def fct_location_revenue_weekly(duckdb: DuckDBResource) -> None:
+    query = """
+    create or replace table fct_location_revenue_weekly as
+    with pricing_inputs as (
+    select
+        account_id,
+        location_id,
+        week_start,
+        billable_employee_count,
+        location_size,
+        is_billable_location
+    from {{ ref('int_location_metrics_weekly') }}
+
+),
+
+pricing_calculated as (
+
+    select
+        account_id,
+        location_id,
+        week_start,
+        billable_employee_count,
+        location_size,
+        is_billable_location,
+
+        case
+            when is_billable_location = false then 0
+            when location_size = 'micro' then 60
+            when location_size = 'small' then 80
+            when location_size = 'large' then 216
+        end as base_price,
+
+        case
+            when is_billable_location = false then 0
+            when billable_employee_count <= 6 then 0
+            when billable_employee_count between 7 and 39 then billable_employee_count - 6
+            else 33
+        end as employees_in_7_39_band,
+
+        case
+            when is_billable_location = false then 0
+            when billable_employee_count <= 39 then 0
+            else billable_employee_count - 39
+        end as employees_in_40_plus_band
+
+    from pricing_inputs
+
+),
+
+final as (
+
+    select
+        account_id,
+        location_id,
+        week_start,
+        billable_employee_count,
+        location_size,
+        is_billable_location,
+        base_price,
+        employees_in_7_39_band,
+        employees_in_40_plus_band,
+        (employees_in_7_39_band * 4) + (employees_in_40_plus_band * 2.4) as extra_employee_cost,
+        base_price + ((employees_in_7_39_band * 4) + (employees_in_40_plus_band * 2.4)) as total_location_revenue
+    from pricing_calculated
+
+)
+
+    select *
+    from final
+    """
+    with duckdb.get_connection() as con:
+        con.execute(query)
+
+
+@dg.asset(
+    deps = ["fct_location_revenue_weekly"]
+)
+def fct_account_billing_weekly(duckdb: DuckDBResource) -> None:
+    query = """
+    create or replace table fct_account_billing_weekly as
+    with account_weekly as (
+
+    select
+        account_id,
+        week_start,
+        count(case when is_billable_location = true then 1 end) as billable_locations,
+        sum(case when is_billable_location = true then billable_employee_count else 0 end) as billable_employees,
+        sum(total_location_revenue) as expected_weekly_revenue
+    from fct_location_revenue_weekly
+    group by account_id, week_start
+
+)
+    select *
+    from account_weekly
+    """
+    with duckdb.get_connection() as con:
+        con.execute(query)
