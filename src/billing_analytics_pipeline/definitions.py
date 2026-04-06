2aed2218a6be21d5d8f3cd95570f202d2e7c42ad
Timmy Ajitoni <ajitonitimileyin@gmail.com>
2026-04-06 18:02:56 +0200

    update definitions.py to include jobs and schedules

--- src/billing_analytics_pipeline/definitions.py
+++ src/billing_analytics_pipeline/definitions.py
@@ -3,6 +3,8 @@ import dagster as dg
 from billing_analytics_pipeline.assets import staging, intermediate, dimensions, facts
 from billing_analytics_pipeline.resources.duckdb_resource import DuckDBResource
 from billing_analytics_pipeline.resources.duckdb_io_manager import duckdb_io_manager
+from billing_analytics_pipeline.jobs.jobs import billing_pipeline_job
+from billing_analytics_pipeline.schedules.schedules import billing_pipeline_schedule
 
 staging_assets = dg.load_assets_from_modules([staging], group_name = "staging")
 intermediate_assets = dg.load_assets_from_modules([intermediate], group_name = "intermediate")
@@ -22,4 +24,6 @@ defs = dg.Definitions(
         "duckdb": DuckDBResource(database="data/warehouse.duckdb"),
         "io_manager": duckdb_io_manager,
     },
+    jobs = [billing_pipeline_job],
+    schedules = [billing_pipeline_schedule],
 )
