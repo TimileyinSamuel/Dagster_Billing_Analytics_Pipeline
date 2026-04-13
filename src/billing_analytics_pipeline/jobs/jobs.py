4ed47d59d59977ee82bdc5e32d85b70a2d93cc40
Timmy Ajitoni <ajitonitimileyin@gmail.com>
2026-04-13 21:52:35 +0200

    edit asset selection to use groups

--- src/billing_analytics_pipeline/jobs/jobs.py
+++ src/billing_analytics_pipeline/jobs/jobs.py
@@ -1,9 +1,9 @@
 import dagster as dg
 from dagster import define_asset_job
 
-all_assets_selection = dg.AssetSelection.all()
+assets_selection = dg.AssetSelection.groups("staging", "intermediate", "mart")
 
 billing_pipeline_job = dg.define_asset_job(
     name = "billing_pipeline_job",
-    selection = all_assets_selection
+    selection = assets_selection
 )
